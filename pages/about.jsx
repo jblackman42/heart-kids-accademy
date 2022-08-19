0ce7ca34e3f0ec169731d75d7be3e9f97370fd1c
Jblack262 <JBlack262@west-mec.org>
2022-08-19 14:14:55 -0700

    meta tags for SEO

--- pages/about.jsx
+++ pages/about.jsx
@@ -4,13 +4,26 @@ import styles from '../styles/pages/About.module.css'
 
 import { Navbar, Banner, TourBanner, Footer } from '../components/index';
 
+import logo from '../assets/HeartKidsAcademyLogo.svg';
 import image from '../assets/example.jpg';
 
 export default function About() {
   return (
     <>
       <Head>
-        <title>About</title>
+        <title>About - Heart Kids Academy</title>
+        <meta name="description" content="At Heart Kids Academy children will experience hands on learning and play in an inspiring, exciting, developmentally appropriate environment. Our mission is to provide a positive foundation for learning in a faith-based environment where learning is fun." />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="About - Heart Kids Academy" />
+        <meta property="og:description" content="At Heart Kids Academy children will experience hands on learning and play in an inspiring, exciting, developmentally appropriate environment. Our mission is to provide a positive foundation for learning in a faith-based environment where learning is fun." />
+        <meta property="og:image" content={logo} />
+        <meta property="og:url" content="https://www.heartkidsacademy.org/" />
+        <meta property="og:site_name" content="HeartKidsAcademy" />
+
+        <meta name="twitter:title" content="About - Heart Kids Academy" />
+        <meta name="twitter:description" content="At Heart Kids Academy children will experience hands on learning and play in an inspiring, exciting, developmentally appropriate environment. Our mission is to provide a positive foundation for learning in a faith-based environment where learning is fun." />
+        <meta name="twitter:image" content={logo} />
       </Head>
       <div className={styles.container}>
         <Navbar />
--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,13 +1,28 @@
 import Head from 'next/head'
 import styles from '../styles/pages/Contact.module.css'
 
+import logo from '../assets/HeartKidsAcademyLogo.svg';
 import { Navbar, Banner, TourBanner, Footer } from '../components/index';
 
 export default function Contact() {
   return (
     <>
       <Head>
-        <title>Contact</title>
+        <Head>
+          <title>Contact Us - Heart Kids Academy</title>
+          <meta name="description" content="At Heart Kids Academy children will experience hands on learning and play in an inspiring, exciting, developmentally appropriate environment. Our mission is to provide a positive foundation for learning in a faith-based environment where learning is fun." />
+          <meta name="robots" content="index, follow" />
+          <meta property="og:type" content="article" />
+          <meta property="og:title" content="Contact Us - Heart Kids Academy" />
+          <meta property="og:description" content="At Heart Kids Academy children will experience hands on learning and play in an inspiring, exciting, developmentally appropriate environment. Our mission is to provide a positive foundation for learning in a faith-based environment where learning is fun." />
+          <meta property="og:image" content={logo} />
+          <meta property="og:url" content="https://www.heartkidsacademy.org/" />
+          <meta property="og:site_name" content="HeartKidsAcademy" />
+
+          <meta name="twitter:title" content="Contact Us - Heart Kids Academy" />
+          <meta name="twitter:description" content="At Heart Kids Academy children will experience hands on learning and play in an inspiring, exciting, developmentally appropriate environment. Our mission is to provide a positive foundation for learning in a faith-based environment where learning is fun." />
+          <meta name="twitter:image" content={logo} />
+        </Head>
       </Head>
       <div className={styles.container}>
         <Navbar />
