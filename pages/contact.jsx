d4c7f1b2d246c0e3383d6cf2c1384bfaddfaace0
Jblack262 <JBlack262@west-mec.org>
2022-08-19 13:39:16 -0700

    contact page rough layout

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,9 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/pages/Home.module.css'
+import styles from '../styles/pages/Contact.module.css'
 
-import Navbar from '../components/Navbar';
-import Banner from '../components/Banner';
+import { Navbar, Banner, TourBanner, Footer } from '../components/index';
 
 export default function Contact() {
   return (
@@ -14,8 +12,24 @@ export default function Contact() {
       <div className={styles.container}>
         <Navbar />
         <Banner />
-        <h1>This is a test</h1>
-        <h1>Contact</h1>
+
+        <div className={styles.contactInfo}>
+          <div className={styles.row}>
+            <h1>Location</h1>
+            <p>Pure Heart Church, Glendale</p>
+          </div>
+          <div className={styles.row}>
+            <h1>Phone</h1>
+            <p>(602) 732 - 6283</p>
+          </div>
+          <div className={styles.row}>
+            <h1>Email</h1>
+            <p>Email@pureheart.org</p>
+          </div>
+        </div>
+
+        <TourBanner />
+        <Footer />
       </div>
     </>
   )
