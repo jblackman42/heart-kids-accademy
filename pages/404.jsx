69df6ac1c9ffb2a10dfabd98fc89735300ac99b2
jblackman42 <jblackman@pureheart.org>
2022-08-23 12:19:46 -0700

    unescaped entities error fix

--- pages/404.jsx
+++ pages/404.jsx
@@ -7,7 +7,7 @@ export default function Custom404() {
           {/* <h1 className="background">404</h1> */}
           <div className={styles.content}>
             <h1>Page not found</h1>
-            <p>We can't seem to find the page you are looking for.</p>
+            <p>{`We can't seem to find the page you are looking for.`}</p>
             <button><Link href="/">Return To Homepage</Link></button>
           </div>
         </div>
